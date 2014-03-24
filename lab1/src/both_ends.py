54c6eae097cc99b9745afd07033384802ab36755
mayuan1992 <mayuan1992@gmail.com>
2014-03-24 13:51:14 +0100

    test

---
+++ lab1/src/both_ends.py
@@ -0,0 +1,14 @@
+def both_ends(string):
+    # if the string has more than 2 chars, return only the first and the last 2
+    if(len(string) > 2):
+        return string[:2] + string[-2:]
+    # else return the empty string
+    else:
+        return ""
+
+if __name__ == '__main__':
+    # get the input string
+    string = raw_input("Insert a string:\n>")
+    
+    # print the computed string
+    print both_ends(string)
